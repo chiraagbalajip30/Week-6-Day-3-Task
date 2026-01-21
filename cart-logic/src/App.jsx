f1b08489973a52193939bd1bf738265b564b5744
chiraagbalajip30 <chiraagbalajip30@gmail.com>
2026-01-21 15:20:51 +0530

    github commit

--- cart-logic/src/App.jsx
+++ cart-logic/src/App.jsx
@@ -3,12 +3,14 @@ import ProductList from "./components/ProductList";
 import { products } from "./data/products";
 import CartProvider from "./context/CartContext";
 import FloatingAddButton from "./components/FloatingAddButton";
+import CartPanel from "./components/CartPanel";
 
 function App() {
   return (
     <CartProvider>
       <div className="bg-gray-100 min-h-screen flex flex-col ">
         <Navbar />
+        <CartPanel />
         <ProductList products={products} />
         <FloatingAddButton />
       </div>
